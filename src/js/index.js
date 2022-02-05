e956ffa7fec49dea06256b22892c643e8101ee5e
Julian Florez <forezjuli08@hotmail.com>
2022-02-05 10:06:00 -0500

    Adding source files

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5]
+const numbersDouble = numbers.map(number => number *2)
+
+class Person {
+	constructor(name) {
+		this.name = name
+  }
+}
---
+++ src/js/second.js
@@ -0,0 +1,2 @@
+const numbers2 = [2, 6, 3, 8, 69, 100]
+const numbersTriple = numbers.map(number => number * 3)
